4e62a5dfadb2e6f97e4803e701a22f8b32cc663a
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-01-30 22:04:33 +0100

    Add AddTask form componemnt

---
+++ src/components/AddTask.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const AddTask = () => {
+  const [text, setText] = useState('')
+  const [day, setDay] = useState('')
+  const [reminder, setReminder] = useState(false)
+
+  console.log(text, day, reminder);
+
+  return (
+    <form className='add-form'>
+      <div className='form-control'>
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add task"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+      </div>
+      <div className='form-control'>
+        <label>Day & Time</label>
+        <input
+          type="text"
+          placeholder="Add day & time"
+          value={day}
+          onChange={e => setDay(e.target.value)}
+        />
+      </div>
+      <div className='form-control form-control-check'>
+        <label>Set reminder</label>
+        <input
+          type="checkbox"
+          value={reminder}
+          onChange={e => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+      <input type="submit" className="btn btn-block" value="save" />
+    </form>
+  )
+}
+
+export default AddTask
