3b0a9faaaecc11554b148adda82081ed03d966af
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-01-31 14:41:07 +0100

    Add http methods GET, POST, DELETE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -27,8 +27,26 @@ function App() {
   ])
   //setTasks([...tasks, {}]) -> aktualizacja state
 
+  //fetching data
+ /*  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTask()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+  
+  const fetchTask = async () => {
+    const res = await fetch('url')
+    const data = await res.json()
+
+    return data
+  } */
+
   //delete task
   const deleteTask = (id) => {
+    /* await fetch(`url/${id}`, {method: 'DELETE',}) */
     setTasks(tasks.filter(t => t.id !== id))
   }
 
@@ -43,6 +61,21 @@ function App() {
     const newTask = { id, ...task }
     setTasks([...tasks, newTask])
   }
+
+  //add task with API
+ /*  const addTask = async (task) => {
+    const res = await fetch('url', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+  } */
 
   return (
     <div className="container">
