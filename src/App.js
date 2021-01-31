2071eda55845a76dee25872f8a5efcc77fc4c151
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-01-31 13:27:00 +0100

    Add addTask handler

--- src/App.js
+++ src/App.js
@@ -36,10 +36,17 @@ function App() {
     setTasks(tasks.map(t => t.id === id ? {...t, reminder: !t.reminder} : t))
   }
 
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
   const [text, setText] = useState('')
   const [day, setDay] = useState('')
   const [reminder, setReminder] = useState(false)
 
-  console.log(text, day, reminder);
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!text) {
+      alert('Please add task')
+      return
+    }
+
+    onAdd({text,day,reminder})
+
+    setText('')
+    setDay('')
+    setReminder(false)
+  }
 
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={onSubmit}>
       <div className='form-control'>
         <label>Task</label>
         <input
@@ -31,6 +44,7 @@ const AddTask = () => {
         <label>Set reminder</label>
         <input
           type="checkbox"
+          checked={reminder}
           value={reminder}
           onChange={e => setReminder(e.currentTarget.checked)}
         />
